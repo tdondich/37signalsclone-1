9f464074ccf666590675c397fe12e991838cc65d
John Duplice <duplicej@gmail.com>
2013-10-01 10:18:09 +0900

    this is the initial resository

---
+++ application.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+    $(".product1").hover(function() {
+		$(".wrapper").hover();
+		$("#basecamp").hover();
+    });
+	$(".product1").hover(function() {
+		$(".wrapper")();
+		$("#basecamp"));
+    });
+
+    $(".product2").hover(function() {
+		$(".wrapper")();
+		$("#highrise")();
+    });
+	$(".product2").hover(function() {
+		$(".wrapper")();
+		$("#highrise")();
+    });
+
+    $(".product3").hover(function() {
+		$(".wrapper")();
+		$("#campfire")();
+    });
+	$(".product3").hover(function() {
+		$(".wrapper")();
+		$("#campfire")();
+    });
+
+});	
+	
+
+	
+
+
+
+
+
